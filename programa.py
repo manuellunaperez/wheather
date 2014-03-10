5bcaeb2c79370c88256efdc1d9ed81833658e338
ManuelLunaPerez <manuellunaperez@gmail.com>
2014-03-10 19:21:22 +0100

    Actualizacion

--- programa.py
+++ programa.py
@@ -1,17 +1,17 @@
 #encoding=utf-8
 import requests
 import json
-provincias = ['','Almeria','Cadiz','Cordoba','Granada','Huelva','Jaen','Malaga','Sevilla']
-print '1. Almería\n2. Cádiz\n3. Córdoba\n4. Granada\n5. Huelva\n6. Jaén\n7. Málaga\n8. Sevilla'
-
-peticion = int(raw_input("¿De qué ciudad quieres saber la temperatura actual? "))
+provincias = ['1. Almería', '2. Cádiz', '3. Córdoba', '4. Granada', '5. Huelva', '6. Jaén', '7. Málaga', '8. Sevilla']
+for provin in provincias:
+	print provin
+peticion = (int(raw_input("¿De qué ciudad quieres saber la temperatura actual? "))-1)
 
 provincia = '%s' % provincias[peticion]
 
-respuesta = requests.get('http://api.openweathermap.org/data/2.5/weather',params={'q':' %s ,spain' % provincia})
+respuesta = requests.get('http://api.openweathermap.org/data/2.5/weather',params={'q':' %s ,spain' % provincia[peticion][3:]})
 
 
 dicc = json.loads(respuesta.text)
 temperatura = dicc['main']['temp']
-
-print 'La temperatura actual de %s es %s grados centígrados' % (provincias[peticion],temperatura-272.15)
+temperatura = round(temperatura - 273)
+print 'La temperatura actual de %s es %s grados centígrados' % (provincias[peticion][3:],temperatura)
